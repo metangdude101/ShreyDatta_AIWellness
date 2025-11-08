21f08c29d19635abf0c551779637e2cb5867ea21
Shrey Datta <146044601+metangdude101@users.noreply.github.com>
2025-11-07 17:44:57 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ const response = await fetch('https://my-first-worker.shreydatta6.workers.dev');
 const data = await response.text(); // Or .text() if the Worker returns plain text
 console.log(data)
 
-document.getElementByID("header")innerHTML = "HELLO!"
+document.getElementByID("header").innerHTML = "HELLO!"
+
 
