2a17a679908d3136332c4bb46b8ecb160f6960ac
Shrey Datta <146044601+metangdude101@users.noreply.github.com>
2025-11-17 20:56:27 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 const response = await fetch('https://my-first-worker.shreydatta6.workers.dev');
-var data = await response.text(); // Or .text() if the Worker returns plain text
+const data = await response.text(); // Or .text() if the Worker returns plain text
 console.log(data)
 
-data.split("AAAAA")
+const splitted = data.split("\n");
 
 const header = document.getElementById("header")
 header.innerHTML = "HELLO!"
 
 const boxes = document.getElementsByClassName("box")
-boxes[0].innerHTML = data[0]
-boxes[1].innerHTML = data[1]
-boxes[2].innerHTML = data[2]
-boxes[3].innerHTML = data[3]
-boxes[4].innerHTML = data[4]
-boxes[5].innerHTML = data[5]
+boxes[0].innerHTML = splitted[0]
+boxes[1].innerHTML = splitted[1]
+boxes[2].innerHTML = splitted[2]
+boxes[3].innerHTML = splitted[3]
+boxes[4].innerHTML = splitted[4]
+boxes[5].innerHTML = splitted[5]
+
 
