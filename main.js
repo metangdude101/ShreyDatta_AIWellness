f10f4a8ce12065b5e2c587e99f514a5fc807eff7
Shrey Datta <146044601+metangdude101@users.noreply.github.com>
2025-11-17 20:48:27 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const response = await fetch('https://my-first-worker.shreydatta6.workers.dev');
-const data = await response.text(); // Or .text() if the Worker returns plain text
+var data = await response.text(); // Or .text() if the Worker returns plain text
 console.log(data)
+
+data.split("\n")
 
 const header = document.getElementById("header")
 header.innerHTML = "HELLO!"
 
 const boxes = document.getElementsByClassName("box")
-boxes[0].innerHTML = data
-
-
+boxes[0].innerHTML = data[0]
+boxes[1].innerHTML = data[1]
+boxes[2].innerHTML = data[2]
+boxes[3].innerHTML = data[3]
+boxes[4].innerHTML = data[4]
+boxes[5].innerHTML = data[5]
