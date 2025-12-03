7910165f28af2bab75ace9660b5857ce557813fa
Shrey Datta <146044601+metangdude101@users.noreply.github.com>
2025-12-02 19:53:41 -0800

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,6 @@ console.log(data)
 
 const splitted = data.split("\n\n");
 
-const header = document.getElementById("header")
-header.innerHTML = "HELLO!"
-
 const boxes = document.getElementsByClassName("box")
 boxes[0].innerHTML = splitted[0]
 boxes[1].innerHTML = splitted[1]
@@ -14,6 +11,7 @@ boxes[2].innerHTML = splitted[2]
 boxes[3].innerHTML = splitted[3]
 boxes[4].innerHTML = splitted[4]
 boxes[5].innerHTML = splitted[5]
+
 
 
 
